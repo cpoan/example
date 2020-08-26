0c9ad52a38dc91e94068f9a9386bc25bb4b49b42
cpoan <chenpoan@outlook.com>
2020-08-26 11:13:41 +0800

    change to correct method

--- helloGit.py
+++ helloGit.py
@@ -2,5 +2,5 @@ print('hello python & GitHub')
 print('I do modification')
 print('third modification')
 print('I wanna make sure that all of these action can be recorded')
-for i in range 10:
+for i in range(10):
     print(i)
