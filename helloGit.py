0a5fbba1c3c147d275638c4afdf1105499f087d3
cpoan <40198026+cpoan@users.noreply.github.com>
2020-10-27 16:15:27 +0800

    Delete helloGit.py

--- helloGit.py
+++
@@ -1,6 +0,0 @@
-print('hello python & GitHub')
-print('I do modification')
-print('third modification')
-print('I wanna make sure that all of these action can be recorded')
-for i in range(10):
-    print(i)
